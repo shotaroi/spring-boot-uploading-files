da73eb85e12248197c843c2fe78a27ccce65c80e
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-16 02:04:39 +0100

    Create project

---
+++ spring-boot-uploading-files/src/main/java/com/shotaroi/springbootuploadingfiles/SpringBootUploadingFilesApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springbootuploadingfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadingFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUploadingFilesApplication.class, args);
+    }
+
+}
---
+++ spring-boot-uploading-files/src/test/java/com/shotaroi/springbootuploadingfiles/SpringBootUploadingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springbootuploadingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUploadingFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
