23af4e1891a3a18969ab4cc1febdc5edf8c14941
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-17 13:54:49 +0100

    Update SpringBootUploadingFilesApplication.java

--- spring-boot-uploading-files/src/main/java/com/shotaroi/springbootuploadingfiles/SpringBootUploadingFilesApplication.java
+++ spring-boot-uploading-files/src/main/java/com/shotaroi/springbootuploadingfiles/SpringBootUploadingFilesApplication.java
@@ -1,13 +1,26 @@
 package com.shotaroi.springbootuploadingfiles;
 
+import com.shotaroi.springbootuploadingfiles.storage.StorageProperties;
+import com.shotaroi.springbootuploadingfiles.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SpringBootUploadingFilesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootUploadingFilesApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
