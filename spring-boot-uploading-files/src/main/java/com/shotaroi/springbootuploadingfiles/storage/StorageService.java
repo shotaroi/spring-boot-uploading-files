60078ca89833706fe823c7730df8383fa1b50861
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-17 13:27:59 +0100

    Update StorageService.java

--- spring-boot-uploading-files/src/main/java/com/shotaroi/springbootuploadingfiles/storage/StorageService.java
+++ spring-boot-uploading-files/src/main/java/com/shotaroi/springbootuploadingfiles/storage/StorageService.java
@@ -1,4 +1,23 @@
 package com.shotaroi.springbootuploadingfiles.storage;
 
-public class StorageService {
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface StorageService {
+
+    void init();
+
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+
 }
